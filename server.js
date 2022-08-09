4161759297541a8e64863b45b9ae04945bfb40ec
Jesus Beltran <jesusbeltran@Jesuss-MBP.attlocal.net>
2022-08-08 18:40:48 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+
+
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads!')
@@ -14,6 +21,12 @@ app.get('/', (req, res) => {
 // Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
+
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
 
 // LISTEN
 app.listen(PORT, () => {
