31da4adf349ad11cb77f94e217ce99a55b32915b
Jesus Beltran <jesusbeltran@Jesuss-MBP.attlocal.net>
2022-08-20 10:35:55 -0700

    added helper methods

--- views/index.jsx
+++ views/index.jsx
@@ -1,32 +1,25 @@
+
 const React = require('react')
 const Default = require('./layouts/Default')
-
-function Index ({breads}) {
+function Index ({breads, title})  {
     return (
-      <Default>
+      <Default title={title}>
         <h2>Index Page</h2>
-        {/* <p>I have {breads[0].name} bread!</p> */}
-        <ul>
-          {
-            breads.map((bread, index)=> {
-              return (
-                <li key={index}>
-                  <a href={`/breads/${index}`}>
-                    {bread.name}
-                  </a>   
-                </li>
-    )
-  })
-}
-
-
-        </ul>
         <div className="newButton">
           <a href="/breads/new"><button>Add a new bread</button></a>
         </div>
-
+        {
+          breads.map((bread, index)=> {
+            return (
+              <li key={bread.id}>
+                <a href={`/breads/${bread.id}`}>
+                  {bread.name}
+                </a>
+              </li>
+            )
+          })
+        }
       </Default>
     )
 }
-
 module.exports = Index
