637816d20966130049707b93ea478112a6a64a91
Jesus Beltran <jesusbeltran@Jesuss-MBP.attlocal.net>
2022-08-20 10:05:09 -0700

    creating schema and model

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const methodOverride =require('method-override')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+const mongoose = require('mongoose')
 
 
 // MIDDLEWARE
@@ -17,6 +18,10 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 
+//mongoose connection
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
 
 
 // ROUTES
